b0f3673a4e304c74ff7a0fa53f846affe3446024
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-08-21 19:47:10 +0200

    add sample xcode project to test swift .gitigonore

---
+++ task1_sample_project/Shared/task1_sample_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  task1_sample_projectApp.swift
+//  Shared
+//
+//  Created by Abdeltawab Mohamed on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct task1_sample_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
