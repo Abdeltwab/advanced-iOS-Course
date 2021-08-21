5db03f5d1023af39553b1cb9e13adc1142696578
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-08-21 22:25:37 +0200

    move task1 sample project to group

---
+++ Task1/Task1_Sample_Project/Task1_Sample_Project/Task1_Sample_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task1_Sample_ProjectApp.swift
+//  Task1_Sample_Project
+//
+//  Created by Abdeltawab Mohamed on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Task1_Sample_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
